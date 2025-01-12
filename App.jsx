1226687a1d955766af48b770f58702ab8e0f2e59
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-01-12 22:22:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar.jsx'
+import Hero from './Components/Hero/Hero.jsx'
+
+const App = () => {
+  return (
+    <main className='overflow-x-hidden bg-white text-dark'>
+      <Hero />
+    </main>
+  )
+}
+
+export default App
